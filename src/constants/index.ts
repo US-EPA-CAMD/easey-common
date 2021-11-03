c82e5252be48cb6e0089fcc7c86777970950a601
Jason Whitehead <jwhitehead@austin.rr.com>
2021-11-03 17:03:50 -0500

    fix: added test2 constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,3 +3,4 @@ export { propertyMetadata } from "./property-metadata.constants";
 export { ApiConfigService } from "./api-config.service";
 
 export const TEST = "TEST";
+export const TEST2 = "TEST2";
