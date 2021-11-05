d233e15d120ab53bfb57020352e811428e6bbe06
Sejal Dhawan <sejaldhawan@cvpcorp.com>
2021-11-05 13:11:17 -0400

    feat(program enums): modify the program enums to include caspar ozone group 3 program (CSOSG3)

--- src/enums/active-allowance-program.enum.ts
+++ src/enums/active-allowance-program.enum.ts
@@ -1,9 +1,10 @@
 export enum ActiveAllowanceProgram {
-  ARP = 'ARP',
-  CSNOX = 'CSNOX',
-  CSOSG1 = 'CSOSG1',
-  CSOSG2 = 'CSOSG2',
-  CSSO2G1 = 'CSSO2G1',
-  CSSO2G2 = 'CSSO2G2',
-  TXSO2 = 'TXSO2',
+  ARP = "ARP",
+  CSNOX = "CSNOX",
+  CSOSG1 = "CSOSG1",
+  CSOSG2 = "CSOSG2",
+  CSOSG3 = "CSOSG3",
+  CSSO2G1 = "CSSO2G1",
+  CSSO2G2 = "CSSO2G2",
+  TXSO2 = "TXSO2",
 }
