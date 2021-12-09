95789aec2739d58c34c8d32db4a00c72e9a62a65
Junie Stigdon <67165079+junie-cvp@users.noreply.github.com>
2021-12-09 13:09:56 -0700

    Revert "Bug/2262-current-date"

--- src/pipes/is-in-date-range.pipe.ts
+++ src/pipes/is-in-date-range.pipe.ts
@@ -2,14 +2,14 @@ import {
   registerDecorator,
   ValidationOptions,
   ValidationArguments,
-} from 'class-validator';
+} from "class-validator";
 
 /**
- * This decorator takes in a min date and 'currentDate' as a parameter
+ * This decorator takes in a min date and max date as a parameter
  * The date range is the min date -> max date inclusive
  */
 export function IsInDateRange(
-  property: [Date, string],
+  property: Date[],
   yearOnly: boolean,
   reportingQuarter: boolean,
   isAnnual: boolean,
@@ -17,7 +17,7 @@ export function IsInDateRange(
 ) {
   return function (object: Object, propertyName: string) {
     registerDecorator({
-      name: 'isInDateRange',
+      name: "isInDateRange",
       target: object.constructor,
       propertyName: propertyName,
       constraints: [property],
@@ -27,7 +27,7 @@ export function IsInDateRange(
           if (value) {
             if (
               isAnnual &&
-              (value === '1980' || value === '1985' || value === '1990')
+              (value === "1980" || value === "1985" || value === "1990")
             ) {
               return true;
             }
@@ -35,42 +35,43 @@ export function IsInDateRange(
               ? new Date(value as number, 0)
               : new Date(value);
             const minDate = args.constraints[0][0];
-            const curDate = (args.object as Date)[args.constraints[0][1]];
-            const curYear = curDate.getFullYear();
+            const maxDate = args.constraints[0][1];
+            const curDate = new Date();
+            const curYear = new Date().getFullYear();
             if (reportingQuarter) {
               if (curDate < new Date(`March 31, ${curYear}`)) {
                 return (
                   dateObject >= minDate &&
-                  dateObject <= curDate &&
+                  dateObject <= maxDate &&
                   dateObject <= new Date(`December 31, ${curYear - 1}`)
                 );
               } else if (curDate < new Date(`June 30, ${curYear}`)) {
                 return (
                   dateObject >= minDate &&
-                  dateObject <= curDate &&
+                  dateObject <= maxDate &&
                   dateObject <= new Date(`March 31, ${curYear}`)
                 );
               } else if (curDate < new Date(`September 30, ${curYear}`)) {
                 return (
                   dateObject >= minDate &&
-                  dateObject <= curDate &&
+                  dateObject <= maxDate &&
                   dateObject <= new Date(`June 30, ${curYear}`)
                 );
               } else if (curDate < new Date(`December 31, ${curYear}`)) {
                 return (
                   dateObject >= minDate &&
-                  dateObject <= curDate &&
+                  dateObject <= maxDate &&
                   dateObject <= new Date(`September 30, ${curYear}`)
                 );
               } else {
                 return (
                   dateObject >= minDate &&
-                  dateObject <= curDate &&
+                  dateObject <= maxDate &&
                   dateObject <= new Date(`December 31, ${curYear}`)
                 );
               }
             }
-            return dateObject >= minDate && dateObject <= curDate;
+            return dateObject >= minDate && dateObject <= maxDate;
           }
           return true;
         },
