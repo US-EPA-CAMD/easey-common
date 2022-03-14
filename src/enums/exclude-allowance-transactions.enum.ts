16d50651b17c3af744792e24e74995a32422eb53
Sejal Dhawan <sejaldhawan@cvpcorp.com>
2022-03-14 16:34:32 -0400

    fix(exclude-allowance-transactions.enum.ts): modify ExcludeAllowanceTransactions

--- src/enums/exclude-allowance-transactions.enum.ts
+++ src/enums/exclude-allowance-transactions.enum.ts
@@ -2,7 +2,7 @@ export enum ExcludeAllowanceTransactions {
   BUY_ACCOUNT_TYPE = "buyAccountType",
   BUY_EPA_REGION = "buyEpaRegion",
   BUY_OWNER = "buyOwner",
-  BUY_SOURCE_CATEGORY = "sellSourceCategory",
+  BUY_SOURCE_CATEGORY = "buySourceCategory",
   BUY_STATE = "buyState",
   SELL_ACCOUNT_TYPE = "sellAccountType",
   SELL_EPA_REGION = "sellEpaRegion",
