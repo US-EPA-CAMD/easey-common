8b28581c9289889a180c820336710cf5ac0d21e6
Kyle-Herceg <kyleherceg@cvpcorp.com>
2022-04-07 12:45:08 -0400

    feat: added streaming service and module
    Added a streaming service and module, as a wrapper around a pg-query-stream

---
+++ src/stream/stream.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { ReadStream } from "fs";
+
+var Pool = require("pg-pool");
+const QueryStream = require("pg-query-stream");
+
+@Injectable()
+export class StreamService {
+  private pool;
+
+  constructor(private readonly configService: ConfigService) {
+    this.pool = new Pool({
+      user: this.configService.get<string>("database.user"),
+      host: this.configService.get<string>("database.host"),
+      database: this.configService.get<string>("database.name"),
+      password: this.configService.get<string>("database.pwd"),
+      port: this.configService.get<number>("database.port"),
+      max: 20, // set pool max size to 20
+      idleTimeoutMillis: 1000, // close idle clients after 1 second
+      connectionTimeoutMillis: 1000, // return an error after 1 second if connection could not be established
+      maxUses: 500, // close (and replace) a connection after it has been used 7500 times
+    });
+  }
+
+  async getStream(query): Promise<ReadStream> {
+    let queryString: string = query[0];
+    const params = query[1];
+
+    let paramIndex = 1;
+    params.forEach((param) => {
+      queryString = queryString.replace("$" + paramIndex, "'" + param + "'");
+      paramIndex++;
+    });
+
+    const client = await this.pool.connect();
+    const queryStream = new QueryStream(queryString);
+
+    return client.query(queryStream);
+  }
+}
