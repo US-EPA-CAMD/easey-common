e2b766b9493bd60d9cb52114a828e35eba510d05
Kyle-Herceg <kyleherceg@cvpcorp.com>
2022-08-05 13:26:58 -0400

    fix: fixed the logging exception to accept object as well as string
    Fixed the logging exception message type to accept a string or an object.

--- src/exceptions/logging.exception.ts
+++ src/exceptions/logging.exception.ts
@@ -3,7 +3,7 @@ import { HttpException } from "@nestjs/common";
 export class LoggingException extends HttpException {
   public metadata: object = {};
 
-  constructor(message: string, status: number, metadata?: object) {
+  constructor(message: string | object, status: number, metadata?: object) {
     super(message, status);
     if (metadata) {
       this.metadata = metadata;
