31c2852981933da75e1d0160dd1e09d29a75908b
Kyle-Herceg <kyleherceg@cvpcorp.com>
2022-09-16 14:45:20 -0400

    feat: added error classification
    Added error classification to logging exception based on status code

--- src/exceptions/logging.exception.ts
+++ src/exceptions/logging.exception.ts
@@ -4,10 +4,39 @@ export class LoggingException extends HttpException {
   public metadata: object = {};
 
   constructor(message: string | object, status: number, metadata?: object) {
+    let errorType;
+
+    switch (status) {
+      case 400:
+        errorType = "Bad Request";
+        break;
+      case 404:
+        errorType = "Not Found";
+        break;
+      case 403:
+        errorType = "Forbidden";
+        break;
+      case 401:
+        errorType = "Unauthorized";
+        break;
+      case 408:
+        errorType = "Request Timeout";
+        break;
+      case 504:
+        errorType = "Gateway Timeout";
+        break;
+      case 502:
+        errorType = "Bad Gateway";
+        break;
+      default:
+        errorType = "Internal Server Error";
+    }
+
     super(
       {
         message: message,
         statusCode: status,
+        error: errorType,
       },
       status
     );
