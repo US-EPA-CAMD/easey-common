26505d25488784a1f35ef7ca332027d106a731ac
subrataroy1 <subrataroy@cvpcorp.com>
2022-11-02 12:21:53 -0400

    fix: update min and max pipes

--- src/pipes/min.pipe.ts
+++ src/pipes/min.pipe.ts
@@ -1,28 +1,28 @@
 import {
-  registerDecorator,
-  ValidationOptions,
-  ValidationArguments,
-  isNumberString,
+	registerDecorator,
+	ValidationOptions,
+	ValidationArguments,
 } from "class-validator";
 
+function min(num, min) {
+	return typeof num === "number" && typeof min === "number" && num >= min;
+}
+
 export function Min(minVal: number, validationOptions?: ValidationOptions) {
-  return function (object: Object, propertyName: string) {
-    registerDecorator({
-      name: "Min",
-      target: object.constructor,
-      propertyName: propertyName,
-      options: validationOptions,
-      validator: {
-        validate(value: any, args: ValidationArguments) {
-          if (value !== null) {
-						return (
-							isNumberString(value, { no_symbols: true }) &&
-							(value as number) >= minVal
-						);
+	return function (object: Object, propertyName: string) {
+		registerDecorator({
+			name: "Min",
+			target: object.constructor,
+			propertyName: propertyName,
+			options: validationOptions,
+			validator: {
+				validate(value: any, args: ValidationArguments) {
+					if (value !== null) {
+						return min(value, minVal);
 					}
-          return true;
-        },
-      },
-    });
-  };
+					return true;
+				},
+			},
+		});
+	};
 }
