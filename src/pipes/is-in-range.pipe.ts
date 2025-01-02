32384cbec27aff41b1138149b9f4f30b0969cf75
ergjustin <justin.joseph.erg.com>
2025-01-02 17:16:49 -0600

    fix: implement matchesRegex and isInRange

--- src/pipes/is-in-range.pipe.ts
+++ src/pipes/is-in-range.pipe.ts
@@ -5,6 +5,58 @@ import {
   isNumberString,
 } from "class-validator";
 
+export function isInRange(value: any, minVal: number, maxVal: number, isMinValValid?: boolean, isMaxValValid?: boolean): boolean {
+  if (value !== null) {
+    if (typeof value === "number") {
+      if (!isMinValValid && !isMaxValValid) {
+        return value > minVal && value < maxVal;
+      }
+
+      if (!isMinValValid) {
+        return value > minVal && value <= maxVal;
+      }
+
+      if (!isMaxValValid) {
+        return value >= minVal && value < maxVal;
+      }
+
+      return value >= minVal && value <= maxVal;
+    }
+
+    if (!isMinValValid && !isMaxValValid) {
+      return (
+        isNumberString(value, { no_symbols: true }) &&
+        (value as number) > minVal &&
+        (value as number) < maxVal
+      );
+    }
+
+    if (!isMinValValid) {
+      return (
+        isNumberString(value, { no_symbols: true }) &&
+        (value as number) > minVal &&
+        (value as number) <= maxVal
+      );
+    }
+
+    if (!isMaxValValid) {
+      return (
+        isNumberString(value, { no_symbols: true }) &&
+        (value as number) >= minVal &&
+        (value as number) < maxVal
+      );
+    }
+
+    return (
+      isNumberString(value, { no_symbols: true }) &&
+      (value as number) >= minVal &&
+      (value as number) <= maxVal
+    );
+  }
+  return true;
+
+}
+
 export function IsInRange(
   minVal: number,
   maxVal: number,
@@ -20,54 +72,7 @@ export function IsInRange(
       options: validationOptions,
       validator: {
         validate(value: any, args: ValidationArguments) {
-          if (value !== null) {
-            if (typeof value === "number") {
-              if (!isMinValValid && !isMaxValValid) {
-                return value > minVal && value < maxVal;
-              }
-
-              if (!isMinValValid) {
-                return value > minVal && value <= maxVal;
-              }
-
-              if (!isMaxValValid) {
-                return value >= minVal && value < maxVal;
-              }
-
-              return value >= minVal && value <= maxVal;
-            }
-
-            if (!isMinValValid && !isMaxValValid) {
-              return (
-                isNumberString(value, { no_symbols: true }) &&
-                (value as number) > minVal &&
-                (value as number) < maxVal
-              );
-            }
-
-            if (!isMinValValid) {
-              return (
-                isNumberString(value, { no_symbols: true }) &&
-                (value as number) > minVal &&
-                (value as number) <= maxVal
-              );
-            }
-
-            if (!isMaxValValid) {
-              return (
-                isNumberString(value, { no_symbols: true }) &&
-                (value as number) >= minVal &&
-                (value as number) < maxVal
-              );
-            }
-
-            return (
-              isNumberString(value, { no_symbols: true }) &&
-              (value as number) >= minVal &&
-              (value as number) <= maxVal
-            );
-          }
-          return true;
+          return isInRange(value, minVal, maxVal, isMinValValid, isMaxValValid);
         },
         defaultMessage: ({ property }: ValidationArguments) => {
           return `The ${property} must be within the range of ${minVal} and ${maxVal}`;
