d01e9aa95f56ae60e2ad03cf00c7b1c180bb5fd9
Jhernandez29 <jonashernandez@erg.com>
2025-10-15 16:18:58 -0500

    fix: updated typeOrm and updating version.

--- src/cors-options/cors-options.module.ts
+++ src/cors-options/cors-options.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CorsOptionsService } from "./cors-options.service";
 
+//Added TypeOrmModule since typeorm was updated
 @Module({
   imports: [TypeOrmModule.forFeature([])],
   providers: [CorsOptionsService],
